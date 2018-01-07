694c64bf051428cb66f747b26f9c3c15fbefb011
Prashant Kumar <prash.kumar047@gmail.com>
2018-01-07 17:53:50 +0530

    minor changes

--- models/userModel.js
+++ models/userModel.js
@@ -1,12 +1,15 @@
 import mongoose, {Schema} from 'mongoose';
 import bcrypt from 'bcrypt';
 
+// Defining the user Schema
 const UserSchema = new Schema({
     userName: {required: true, type: String},
     password: {required: true, type: String},
     email: {required: true, type: String}
-},{collection: 'user'});
+},
+{collection: 'user'}); // Name of the collection on the database
 
+// Hashes the password before saving to database
 UserSchema.pre('save',function(next){
     var user = this;
     bcrypt.hash(user.password, 10)
@@ -20,10 +23,11 @@ UserSchema.pre('save',function(next){
         });
 });
 
+// compares hashed password against input password
 UserSchema.methods.comparePassword = function(password){
     let user = this;
     return bcrypt.compareSync(password, user.password);
 };
 
-const User = mongoose.model('user', UserSchema);
+const User = mongoose.model('user', UserSchema); // defining the user model
 export {User};
